0aaa951ac97f08a2b4d205733a77998bee9e1373
James Martin <jamesmartin442@gmail.com>
2020-01-19 11:47:26 +0000

    Admins can now add points for any dog

--- functions/funcs/addRun.js
+++ functions/funcs/addRun.js
@@ -1,53 +1,77 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+// function isAdmin() {
+// 	return signedIn() && request.auth.uid in get(/databases/$(database)/documents/meta/admin).data.adminUids;
+//   }
+
+async function isAdmin(uid) {
+  try {
+    const db = admin.firestore();
+    const admins = db.collection("meta").doc("admin");
+    const doc = await admins.get();
+    const { adminUids } = doc.data();
+    if (adminUids && adminUids.includes(uid)) {
+      console.log("Admin is updating a record");
+      return true;
+    }
+
+    return false;
+  } catch (e) {
+    console.error("Error in admin check", e.message);
+    return false;
+  }
+}
 
 module.exports = functions.https.onCall(async (postData, context) => {
-	console.log(postData);
-	const db = admin.firestore();
-	const docId = postData.docId;
-	const data = { ...postData };
-	delete data.docId;
+  console.log(postData);
+  const db = admin.firestore();
+  const docId = postData.docId;
+  const data = { ...postData };
+  delete data.docId;
 
-	const leagueRef = db.collection('leagues').doc(data.league);
-	const dogRef = db.collection('dogs').doc(data.dogId);
+  const leagueRef = db.collection("leagues").doc(data.league);
+  const dogRef = db.collection("dogs").doc(data.dogId);
 
-	let docRef;
+  let docRef;
 
-	if (docId) {
-		docRef = db.collection(`dogs/${data.dogId}/runs`).doc(docId);
-	} else {
-		docRef = db.collection(`dogs/${data.dogId}/runs`).doc();
-	}
+  if (docId) {
+    docRef = db.collection(`dogs/${data.dogId}/runs`).doc(docId);
+  } else {
+    docRef = db.collection(`dogs/${data.dogId}/runs`).doc();
+  }
 
-	const leagueSnap = await leagueRef.get();
-	const leagueData = leagueSnap.data();
-	// If the league is closed, then points can no longer be added
-	if (!leagueData.open) {
-		throw new functions.https.HttpsError(
-			'league-closed',
-			'The league is closed, you cannot add points'
-		);
-	}
+  const leagueSnap = await leagueRef.get();
+  const leagueData = leagueSnap.data();
+  // If the league is closed, then points can no longer be added
+  if (!leagueData.open) {
+    throw new functions.https.HttpsError(
+      "league-closed",
+      "The league is closed, you cannot add points"
+    );
+  }
 
-	data.leagueName = leagueData.name;
-	data.leagueSport = leagueData.sport;
+  data.leagueName = leagueData.name;
+  data.leagueSport = leagueData.sport;
 
-	const dogSnap = await dogRef.get();
-	const dogData = dogSnap.data();
+  const dogSnap = await dogRef.get();
+  const dogData = dogSnap.data();
 
-	// If the dog is now owned by the current user, abort
-	if (dogData.uid !== context.auth.uid) {
-		throw new functions.https.HttpsError(
-			'not-your-dog',
-			'You are trying to edit a dog that is not your own'
-		);
-	}
+  // If the dog is not owned by the current user, abort
+  if (dogData.uid !== context.auth.uid) {
+    if (!(await isAdmin(context.auth.uid))) {
+      throw new functions.https.HttpsError(
+        "not-your-dog",
+        "You are trying to edit a dog that is not your own"
+      );
+    }
+  }
 
-	if (docId) {
-		docRef.update(data);
-	} else {
-		docRef.set(data);
-	}
+  if (docId) {
+    docRef.update(data);
+  } else {
+    docRef.set(data);
+  }
 
-	return true;
+  return true;
 });
--- src/components/leagues/League.tsx
+++ src/components/leagues/League.tsx
@@ -1,153 +1,161 @@
-import React, { useState } from 'react';
-import { useDoc } from '@/hooks/firebase';
-import Loading from '@/components/Loading';
-import Error from '@/components/Error';
-import { Button, Modal, List, Popconfirm, Collapse, Switch } from 'antd';
-import DogSelector from '@/components/admin/DogSelector';
-import firebase, { firestore } from 'firebase/app';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import { useDoc } from "@/hooks/firebase";
+import Loading from "@/components/Loading";
+import Error from "@/components/Error";
+import { Button, Modal, List, Popconfirm, Collapse, Switch } from "antd";
+import DogSelector from "@/components/admin/DogSelector";
+import firebase, { firestore } from "firebase/app";
+import styled from "styled-components";
+import useRouter from "use-react-router";
 
 const FloatRight = styled.div`
-	float: right;
+  float: right;
 `;
 
 interface DogProps {
-	id: string;
-	grade: Number;
-	remove: (doc: firestore.DocumentSnapshot) => void;
+  id: string;
+  grade: Number;
+  remove: (doc: firestore.DocumentSnapshot) => void;
 }
 
 function Dog({ id, grade, remove }: DogProps) {
-	const [value, loading, error] = useDoc('dogs', id);
+  const { history } = useRouter();
+  const [value, loading, error] = useDoc("dogs", id);
 
-	if (loading || error || !value) return <Loading />;
+  function goToLogPoints(): void {
+    if (!id) return;
+    history.push(`/points/${id}`);
+  }
 
-	const data = value.data();
+  if (loading || error || !value) return <Loading />;
 
-	if (!data) return <Error error={`No dog with for ${id}`} />;
+  const data = value.data();
 
-	return (
-		<p>
-			{data.name} (Grade: {grade}){' '}
-			<Popconfirm
-				title="Are you sure you want to delete this dog? It is not possible to reverse this."
-				onConfirm={remove.bind(null, value)}
-			>
-				<Button icon="delete" type="danger" shape="circle" />
-			</Popconfirm>
-		</p>
-	);
+  if (!data) return <Error error={`No dog with for ${id}`} />;
+
+  return (
+    <List.Item
+      actions={[
+        <Button onClick={goToLogPoints}>Add points</Button>,
+        <Popconfirm
+          title="Are you sure you want to delete this dog? It is not possible to reverse this."
+          onConfirm={remove.bind(null, value)}
+        >
+          <Button icon="delete" type="danger">
+            Delete
+          </Button>
+        </Popconfirm>
+      ]}
+    >
+      {data.name} (Grade: {grade}){" "}
+    </List.Item>
+  );
 }
 
 interface LeagueProps {
-	doc: firestore.DocumentSnapshot;
+  doc: firestore.DocumentSnapshot;
 }
 export default function League({ doc }: LeagueProps) {
-	const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
-	const data = doc.data();
-	const dogs =
-		(data && data.dogs && Object.entries<{ grade: number }>(data.dogs)) ||
-		[];
+  const data = doc.data();
+  const dogs =
+    (data && data.dogs && Object.entries<{ grade: number }>(data.dogs)) || [];
 
-	if (!data) return <Loading />;
+  if (!data) return <Loading />;
 
-	function openModal() {
-		setModalOpen(true);
-	}
+  function openModal() {
+    setModalOpen(true);
+  }
 
-	function closeModal() {
-		setModalOpen(false);
-	}
+  function closeModal() {
+    setModalOpen(false);
+  }
 
-	function onSelect(dog: firestore.DocumentSnapshot) {
-		const dogData = dog.data();
-		const leagueData = doc.data();
+  function onSelect(dog: firestore.DocumentSnapshot) {
+    const dogData = dog.data();
+    const leagueData = doc.data();
 
-		if (!dogData || !leagueData) return;
+    if (!dogData || !leagueData) return;
 
-		const entry = { grade: dogData.grade };
+    const entry = { grade: dogData.grade };
 
-		const leagueDogs = { ...leagueData.dogs, [dog.id]: entry };
+    const leagueDogs = { ...leagueData.dogs, [dog.id]: entry };
 
-		firebase
-			.firestore()
-			.collection('leagues')
-			.doc(doc.id)
-			.update({ dogs: leagueDogs });
-	}
+    firebase
+      .firestore()
+      .collection("leagues")
+      .doc(doc.id)
+      .update({ dogs: leagueDogs });
+  }
 
-	function removeDogFromLeague(dog: firestore.DocumentSnapshot) {
-		const leagueData = doc.data();
+  function removeDogFromLeague(dog: firestore.DocumentSnapshot) {
+    const leagueData = doc.data();
 
-		if (!leagueData) return;
+    if (!leagueData) return;
 
-		const leagueDogs = { ...leagueData.dogs };
+    const leagueDogs = { ...leagueData.dogs };
 
-		delete leagueDogs[dog.id];
+    delete leagueDogs[dog.id];
 
-		firebase
-			.firestore()
-			.collection('leagues')
-			.doc(doc.id)
-			.update({ dogs: leagueDogs });
-	}
+    firebase
+      .firestore()
+      .collection("leagues")
+      .doc(doc.id)
+      .update({ dogs: leagueDogs });
+  }
 
-	function toggleOpen(state: boolean, e: MouseEvent) {
-		e.stopPropagation();
-		firebase
-			.firestore()
-			.collection('leagues')
-			.doc(doc.id)
-			.update({ open: state });
-	}
+  function toggleOpen(state: boolean, e: MouseEvent) {
+    e.stopPropagation();
+    firebase
+      .firestore()
+      .collection("leagues")
+      .doc(doc.id)
+      .update({ open: state });
+  }
 
-	return (
-		<Collapse>
-			<Collapse.Panel
-				key="id"
-				header={
-					<h2>
-						{data.name} ({data.sport})
-						<FloatRight>
-							<Switch
-								unCheckedChildren="Closed"
-								checkedChildren="Open"
-								checked={data.open}
-								onChange={toggleOpen}
-							/>
-						</FloatRight>
-					</h2>
-				}
-			>
-				<Button
-					type="primary"
-					icon="plus"
-					onClick={openModal}
-					block
-					disabled={!data.open}
-				>
-					Add dogs
-				</Button>
-				<List
-					dataSource={dogs}
-					renderItem={([dog, details]) => (
-						<Dog
-							id={dog}
-							grade={details.grade}
-							remove={removeDogFromLeague}
-						/>
-					)}
-				/>
-				<Modal
-					title="Select a dog"
-					visible={modalOpen}
-					onOk={closeModal}
-					onCancel={closeModal}
-				>
-					<DogSelector leagueId={doc.id} onSelect={onSelect} />
-				</Modal>
-			</Collapse.Panel>
-		</Collapse>
-	);
+  return (
+    <Collapse>
+      <Collapse.Panel
+        key="id"
+        header={
+          <h2>
+            {data.name} ({data.sport})
+            <FloatRight>
+              <Switch
+                unCheckedChildren="Closed"
+                checkedChildren="Open"
+                checked={data.open}
+                onChange={toggleOpen}
+              />
+            </FloatRight>
+          </h2>
+        }
+      >
+        <Button
+          type="primary"
+          icon="plus"
+          onClick={openModal}
+          block
+          disabled={!data.open}
+        >
+          Add dogs
+        </Button>
+        <List
+          dataSource={dogs}
+          renderItem={([dog, details]) => (
+            <Dog id={dog} grade={details.grade} remove={removeDogFromLeague} />
+          )}
+        />
+        <Modal
+          title="Select a dog"
+          visible={modalOpen}
+          onOk={closeModal}
+          onCancel={closeModal}
+        >
+          <DogSelector leagueId={doc.id} onSelect={onSelect} />
+        </Modal>
+      </Collapse.Panel>
+    </Collapse>
+  );
 }
