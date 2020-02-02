7152aee87ca49accbb82de195e24e1e60b89887b
James Martin <jamesmartin442@gmail.com>
2020-02-02 14:19:44 +0000

    Fix adding league with undefined

--- src/components/leagues/Form.tsx
+++ src/components/leagues/Form.tsx
@@ -1,86 +1,102 @@
-import React, { FormEvent } from 'react';
-import { Form, Button, Input, Select, Switch } from 'antd';
-import { types } from '@/utils/sportTypes';
-import firebase, { firestore } from 'firebase/app';
-import { FormComponentProps } from 'antd/lib/form/Form';
+import React, { FormEvent } from "react";
+import { Form, Button, Input, Select, Switch } from "antd";
+import { types } from "@/utils/sportTypes";
+import firebase, { firestore } from "firebase/app";
+import { FormComponentProps } from "antd/lib/form/Form";
 
 interface Props {
-	doc?: firestore.DocumentReference;
-	onSave: () => void;
+  doc?: firestore.DocumentReference;
+  onSave: () => void;
 }
 
 function LeagueForm({ form, doc, onSave }: Props & FormComponentProps) {
-	const { getFieldDecorator } = form;
+  const { getFieldDecorator } = form;
 
-	function submit(e: FormEvent) {
-		e.preventDefault();
-		form.validateFields((err, values) => {
-			if (!err) {
-				console.log(values);
-				const db = firebase.firestore().collection(`leagues`);
+  function submit(e: FormEvent) {
+    e.preventDefault();
+    form.validateFields((err, values) => {
+      if (!err) {
+        values.open = values.open || false;
+        values.currentLeague = values.currentLeague || false;
+        console.log(values);
+        const db = firebase.firestore().collection(`leagues`);
 
-				const docToUse = doc && doc.id ? db.doc(doc.id) : db.doc();
-				const open = typeof values.open === undefined ? false : true;
+        const docToUse = doc && doc.id ? db.doc(doc.id) : db.doc();
+        const open = typeof values.open === undefined ? false : true;
 
-				const data = { ...values, open };
+        const data = { ...values, open };
 
-				if (doc) {
-					docToUse.update(data);
-				} else {
-					docToUse.set(data);
-				}
+        if (doc) {
+          docToUse.update(data);
+        } else {
+          docToUse.set(data);
+        }
 
-				form.resetFields();
-				if (onSave && typeof onSave === 'function') {
-					onSave();
-				}
-			}
-		});
-	}
+        form.resetFields();
+        if (onSave && typeof onSave === "function") {
+          onSave();
+        }
+      }
+    });
+  }
 
-	return (
-		<Form onSubmit={submit} layout="vertical">
-			<Form.Item label="League name">
-				{getFieldDecorator('name', {
-					rules: [
-						{
-							required: true,
-							message: 'You must provide a name'
-						}
-					]
-				})(<Input />)}
-			</Form.Item>
+  return (
+    <Form onSubmit={submit} layout="vertical">
+      <Form.Item label="League name">
+        {getFieldDecorator("name", {
+          rules: [
+            {
+              required: true,
+              message: "You must provide a name"
+            }
+          ]
+        })(<Input />)}
+      </Form.Item>
 
-			<Form.Item label="Sport">
-				{getFieldDecorator('sport', {
-					rules: [
-						{
-							required: true,
-							message: 'Please pick a sport'
-						}
-					]
-				})(
-					<Select>
-						{types.map((type) => (
-							<Select.Option key={type} value={type}>
-								{type}
-							</Select.Option>
-						))}
-					</Select>
-				)}
-			</Form.Item>
-			<Form.Item label="Open or closed">
-				{getFieldDecorator('open')(
-					<Switch checkedChildren="Open" unCheckedChildren="Closed" />
-				)}
-			</Form.Item>
-			<Button htmlType="submit" type="primary" block>
-				Save
-			</Button>
-		</Form>
-	);
+      <Form.Item label="Sport">
+        {getFieldDecorator("sport", {
+          rules: [
+            {
+              required: true,
+              message: "Please pick a sport"
+            }
+          ]
+        })(
+          <Select>
+            {types.map(type => (
+              <Select.Option key={type} value={type}>
+                {type}
+              </Select.Option>
+            ))}
+          </Select>
+        )}
+      </Form.Item>
+      <Form.Item label="Open or closed">
+        {getFieldDecorator("open")(
+          <Switch
+            checkedChildren="Open"
+            unCheckedChildren="Closed"
+            checked={false}
+          />
+        )}
+      </Form.Item>
+
+      <Form.Item label="Is current league?">
+        {getFieldDecorator("currentLeague")(
+          <Switch
+            checkedChildren="Yes"
+            unCheckedChildren="No"
+            checked={false}
+          />
+        )}
+      </Form.Item>
+      <Button htmlType="submit" type="primary" block>
+        Save
+      </Button>
+    </Form>
+  );
 }
 
-export default Form.create<Props & FormComponentProps>({ name: 'leagueForm' })(
-	LeagueForm
+export default Form.create<Props & FormComponentProps>({ name: "leagueForm" })(
+  LeagueForm
 );
